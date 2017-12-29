2e7626d1f0ddf87b6a61eb19eadbf92b67e3aa03
WangLei <nihiue@kesci.com>
2017-12-29 17:29:12 +0800

    done

---
+++ MyJumpHelper/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace MyJumpHelper
+{
+    partial class JumpHelper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(48, 66);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(145, 51);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(217, 66);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(145, 51);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ready";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 12);
+            this.label4.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 21);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "2.5";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(201, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.setRatio_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(48, 160);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(314, 87);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Conversion Ratio";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(48, 296);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(314, 163);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Status";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(349, 476);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "By Nihiue";
+            // 
+            // JumpHelper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 497);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Name = "JumpHelper";
+            this.Text = "JumpHelper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ MyJumpHelper/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Diagnostics;
+using Microsoft.Win32;
+using CSharpKeyboardHook;
+namespace MyJumpHelper
+{
+    public partial class JumpHelper : Form
+    {
+        private KeyboardHookLib _keyboardHook = null;
+
+        private int startX=0;
+        private int startY=0;
+        private int endX=0;
+        private int endY = 0;
+        private bool isReadyToJump = false;
+        private double convRatio = 2.5;
+
+        public JumpHelper()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            if (_keyboardHook == null)
+            {
+                _keyboardHook = new KeyboardHookLib();
+                _keyboardHook.InstallHook(this.KeyPress);
+                label2.Text = "Running";
+            }
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            if (_keyboardHook != null)
+            {
+                _keyboardHook.UninstallHook();
+                label2.Text = "Ready";
+                _keyboardHook = null;
+            }
+        }
+
+        private bool sendTouchCmd(int duration)
+        {
+            bool result = false;
+            try
+            {
+                using (Process myPro = new Process())
+                {
+                    myPro.StartInfo.FileName = "cmd.exe";
+                    myPro.StartInfo.UseShellExecute = false;
+                    myPro.StartInfo.RedirectStandardInput = true;
+                    myPro.StartInfo.RedirectStandardOutput = true;
+                    myPro.StartInfo.RedirectStandardError = true;
+                    myPro.StartInfo.CreateNoWindow = true;
+                    myPro.Start();                    
+                    string str = string.Format(@"{0} {1} {2}", "adb shell input swipe 300 300 300 300", duration, "&exit");
+
+                    myPro.StandardInput.WriteLine(str);
+                    myPro.StandardInput.AutoFlush = true;
+                    myPro.WaitForExit();
+
+                    result = true;
+                }
+            }
+            catch
+            {
+
+            }
+            return result;
+        }
+
+        public void KeyPress(KeyboardHookLib.HookStruct hookStruct, out bool handle)
+        {
+            handle = false;
+
+            switch (hookStruct.vkCode)
+            {
+                case 49: // number key 1
+                    this.startX = Control.MousePosition.X;
+                    this.startY = Control.MousePosition.Y;
+                    label1.Text = string.Format(@"Start: ({0},{1})", this.startX, this.startY);
+                    this.isReadyToJump = true;
+                    break;
+                case 50: // number key 2
+                    this.endX = Control.MousePosition.X;
+                    this.endY = Control.MousePosition.Y;
+
+                    label3.Text = string.Format(@"End: ({0},{1})", this.endX, this.endY);
+        
+                    if (!this.isReadyToJump)
+                    {
+                        return;
+                    }
+                    this.isReadyToJump = false;
+                    int delta =(int) Math.Sqrt((this.startX - this.endX) * (this.startX - this.endX) + (this.startY - this.endY) * (this.startY - this.endY));
+                    int duration = (int)(delta * this.convRatio);
+                    label4.Text = string.Format(@"Delta: {0} px, Ratio: {1}, Duration: {2} ms", delta, this.convRatio, duration);
+                    this.sendTouchCmd(duration);
+                    break;                
+                    
+            }           
+        }
+
+        private void setRatio_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double value = double.Parse(this.textBox1.Text);
+                if (value > 0 && value < 20)
+                {
+                    this.convRatio = value;
+                }
+                else
+                {
+                    
+                     MessageBox.Show("Input too large or too small");
+                    
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Not a valid number");
+            }
+           
+
+
+        }
+
+    }
+}
